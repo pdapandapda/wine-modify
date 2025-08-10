e65d27ee5c0abca84cfb91807c394ae33cd2ba02
pdapandapda <linyuqiang55@gmail.com>
2025-08-11 07:27:25 +0800

    Update vulkan.c
    disable VK_EXT_external_memory_host

--- dlls/winevulkan/vulkan.c
+++ dlls/winevulkan/vulkan.c
@@ -498,9 +498,10 @@ static VkResult wine_vk_physical_device_init(struct wine_phys_dev *object, VkPhy
         {
             TRACE("Skipping extension '%s', no implementation found in winevulkan.\n", host_properties[i].extensionName);
         }
-        if (!strcmp(host_properties[i].extensionName, "VK_EXT_external_memory_host"))
-            have_external_memory_host = TRUE;
-        else if (!strcmp(host_properties[i].extensionName, "VK_EXT_map_memory_placed"))
+        //if (!strcmp(host_properties[i].extensionName, "VK_EXT_external_memory_host"))
+            //have_external_memory_host = TRUE;
+        //else 
+        if (!strcmp(host_properties[i].extensionName, "VK_EXT_map_memory_placed"))
             have_memory_placed = TRUE;
         else if (!strcmp(host_properties[i].extensionName, "VK_KHR_map_memory2"))
             have_map_memory2 = TRUE;
@@ -830,8 +831,8 @@ static VkResult wine_vk_device_convert_create_info(VkPhysicalDevice client_physi
     {
         if (!find_extension(extensions, extensions_count, "VK_KHR_external_memory"))
             extra_extensions[extra_count++] = "VK_KHR_external_memory";
-        if (!find_extension(extensions, extensions_count, "VK_EXT_external_memory_host"))
-            extra_extensions[extra_count++] = "VK_EXT_external_memory_host";
+        //if (!find_extension(extensions, extensions_count, "VK_EXT_external_memory_host"))
+            //extra_extensions[extra_count++] = "VK_EXT_external_memory_host";
     }
 
     if (extra_count)
